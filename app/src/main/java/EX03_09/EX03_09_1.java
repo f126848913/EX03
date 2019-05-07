2dfcd3834a3339dbb45d915ccc5dd644940b066e
你的名字 <email>
2019-05-07 13:00:56 +0800

    Initial commit

---
+++ app/src/main/java/EX03_09/EX03_09_1.java
@@ -0,0 +1,25 @@
+package EX03_09;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+import com.example.myapplicati.R;
+
+public class EX03_09_1 extends Activity {
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainlayout);
+
+    }
+    public void onclick(View v){
+        Intent intent=new Intent();
+        intent.setClass(EX03_09_1.this,EX03_09.class);
+        startActivity(intent);
+        EX03_09_1.this.finish();
+    }
+}
